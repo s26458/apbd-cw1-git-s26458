3d360940ee4a47ed67b110bb2e6f7bc25051e534
Eligiusz Zukowski <s26458@pjwstk.edu.pl>
2026-03-29 21:49:07 +0200

    Menu and main loop

--- APBD1/src/Main.java
+++ APBD1/src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("==== welcome to console calculator ====");
+
+        while(true){
+            System.out.println(" --- choose option ---");
+            System.out.println(" 1. ADD");
+            System.out.println(" 2. SUBTRACT");
+            System.out.println(" 3. MULTIPLY");
+            System.out.println(" 4. DIVIDE");
+            System.out.println(" 5. EXIT");
+
+            Scanner s = new Scanner(System.in);
+            int operation = s.nextInt();
+
+            switch(operation){
+                case 1 -> System.out.println("add");
+                case 2 -> System.out.println("subtract");
+                case 3 -> System.out.println("multiply");
+                case 4 -> System.out.println("divide");
+                case 5 -> System.exit(0);
+            }
+
+
         }
     }
 }
