64213732dc7f07968c19ec295ad883ab0d15841d
Eligiusz Zukowski <s26458@pjwstk.edu.pl>
2026-03-29 22:08:51 +0200

    Add archive

--- APBD1/src/Main.java
+++ APBD1/src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        String[] archive = new String[10];
+
         System.out.println("==== welcome to console calculator ====");
 
         while(true){
@@ -11,22 +13,80 @@ public class Main {
             System.out.println(" 2. SUBTRACT");
             System.out.println(" 3. MULTIPLY");
             System.out.println(" 4. DIVIDE");
-            System.out.println(" 5. EXIT");
+            System.out.println(" 5. HISTORY");
+            System.out.println(" 6. EXIT");
 
             Scanner s = new Scanner(System.in);
             int operation = s.nextInt();
 
-            System.out.print("Please enter the first number: ");
-            int firstNumber = s.nextInt();
-            System.out.print("Please enter the second number: ");
-            int secondNumber = s.nextInt();
+
 
             switch(operation){
-                case 1 -> System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
-                case 2 -> System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
-                case 3 -> System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
-                case 4 -> System.out.println(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
-                case 5 -> System.exit(0);
+                case 1 -> add(archive);
+                case 2 -> substract(archive);
+                case 3 -> multiply(archive);
+                case 4 -> divide(archive);
+                case 5 -> {
+                    for (String data : archive) {
+                        if (data != null) {
+                            System.out.println(data);
+                        }
+                    }
+                }
+                case 6 -> System.exit(0);
+                default -> System.out.println("Invalid input");
+            }
+
+
+        }
+    }
+
+    private static void substract(String[] archive) {
+    }
+
+    private static void add(String[] archive){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        int firstNumber = s.nextInt();
+        System.out.print("Please enter the second number: ");
+        int secondNumber = s.nextInt();
+        String data = (firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+        print_and_save(data, archive);
+    }
+    private void subtract(String [] archive){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        int firstNumber = s.nextInt();
+        System.out.print("Please enter the second number: ");
+        int secondNumber = s.nextInt();
+        String data = (firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+        print_and_save(data, archive);
+    }
+    private static void multiply(String[] archive){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        int firstNumber = s.nextInt();
+        System.out.print("Please enter the second number: ");
+        int secondNumber = s.nextInt();
+        String data = (firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+        print_and_save(data, archive);
+    }
+    private static void divide(String[] archive){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        int firstNumber = s.nextInt();
+        System.out.print("Please enter the second number: ");
+        int secondNumber = s.nextInt();
+        String data = (firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+        print_and_save(data, archive);
+    }
+    private static void print_and_save(String data, String[] archive){
+        System.out.println(data);
+        for (int i = 0; i < archive.length; i++){
+            System.out.println(archive[i]);
+            if (archive[i] == null){
+                archive[i] = data;
+                break;
             }
         }
     }
